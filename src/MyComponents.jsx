e8ad60397a757e40fd0dbb8c1bb14c8dda8072bd
Augustine Gyan <augustinehappysunshine369@gmail.com>
2025-02-17 23:08:45 +0000

    array update

--- src/MyComponents.jsx
+++ src/MyComponents.jsx
@@ -36,9 +36,7 @@
 
 
 
-import { useState } from "react";
 
-function MyComponents (){
     // const [name, setName] = useState("nothing");
     // const [quantity, setQuantity] = useState(1);
     // const [comment, setComment]= useState("");
@@ -107,25 +105,36 @@ function MyComponents (){
 
     //UPDATE Array in STATE 
 
-    const [food, setFood] = useState(["APPLE","ORANGE","BANANA"])
+    import React, { useState } from 'react';
+
+function MyComponent() {
+
+    const [foods, setFoods] = useState(["Apple", "Orange", "Banana"]);
 
     function handleAddFood(){
 
+        const newFood = document.getElementById("foodInput").value;
+        document.getElementById("foodInput").value = "";
+        
+        setFoods(f => [...f, newFood]);
     }
-    function handleRemoveFood(){
 
+    function handleRemoveFood(index){
+        
+        setFoods(foods.filter((_, i) => i !== index));
     }
 
-    return(
-    <div>
-        <h2>List of Food</h2>
-        <ul>
-            {food.map((food, index) => <li key={index}>{food}</li>)}
-        </ul>
-    </div>
-    )
+    return (<div>
+                <h2>List of Food</h2>
+                <ul>
+                    {foods.map((food, index) => 
+                    <li key={index} onClick={() => handleRemoveFood(index)}>
+                        {food}
+                    </li>)}
+                </ul>
+                <input type="text" id="foodInput" placeholder="Enter food name"/>
+                <button onClick={handleAddFood}>Add Food</button>
+            </div>);
 }
-export default MyComponents
 
-
-
+export default MyComponent;
